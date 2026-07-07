c6eb3cf6aacd4d4e5ed07e6e106f4aeb63148fe3
code-YuJun <2594131385@qq.com>
2026-07-07 17:50:39 +0800

    feat: 重置项目

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import { viteMockServe } from 'vite-plugin-mock'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    viteMockServe({
-      mockPath: 'src/mock',
-      enable: true,
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
