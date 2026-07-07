b95ba494454efd3096c5283d3df50c5413e60a80
code-YuJun <2594131385@qq.com>
2026-07-07 17:46:02 +0800

    feat: 项目完善

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { viteMockServe } from 'vite-plugin-mock'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteMockServe({
+      mockPath: 'src/mock',
+      enable: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
